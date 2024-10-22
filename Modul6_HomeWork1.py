01d5bc1e6ff54f7a5c488d76a0019f220062e44b
Katana-devel <egorpilipenko0@gmail.com>
2024-10-22 18:59:30 +0300

    Create Modul6_HomeWork1.py

---
+++ Modul6_HomeWork1.py
@@ -0,0 +1,101 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Phone(Field):
+    def __init__(self, phone):
+        if len(phone) == 10:
+            super().__init__(phone)
+        else:
+            raise ValueError("Phone number must contain exactly 10 digits.")
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self,phone_number):
+        new_phone = Phone(phone_number)
+        self.phones.append(new_phone)
+
+
+    def remove_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                self.phones.remove(phone)
+                break
+
+    def edit_phone(self, old_number, new_number):
+        try:
+            for phone in self.phones:
+                if phone.value == old_number:
+                    phone.value = new_number  
+                    break
+        except ValueError as e:
+            print(e)
+
+    def find_phone(self, phone_number):
+        try:
+            for phone in self.phones:
+                if phone.value == phone_number:
+                    return phone
+            return None
+        except ValueError:
+            return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record: Record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            self.data.pop(name)
+    def __str__(self):
+        return f"information about contact: {self.data.items()}"
+
+# Створення нової адресної книги
+book = AddressBook()
+
+# Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+# Додавання запису John до адресної книги
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+# Виведення всіх записів у книзі
+
+print(book)
+
+# Знаходження та редагування телефону для John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
+
+# Видалення запису Jane
+book.delete("Jane")
